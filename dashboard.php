e3f7088f3ca4c8f775121555c722d28bb3adfa9b
Yuvashree S <syuvashree579@gmail.com>
2026-01-31 21:01:55 +0530

    Create dashboard.php for navigation links

---
+++ dashboard.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Dashboard</title>
+</head>
+<body>
+  <h2>Dashboard</h2>
+  <a href="add_student.php">Add Student</a><br><br>
+  <a href="mark_attendance.php">Mark Attendance</a><br><br>
+  <a href="view_attendance.php">View Attendance</a>
+</body>
+</html>
